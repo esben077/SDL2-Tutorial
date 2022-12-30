dc81510fbcbf515394f553aea0bdf752dad99686
brodersalza <esben077@gmail.com>
2022-12-31 00:21:26 +0100

    8: collision - added random stones

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 // #include <iostream>
 //#include <SDL2/SDL.h>
+#include <ctime>
+// #include <cstdlib>s
+#include <vector>
 #include "item.hpp"
 //mingw32-make
-const int WIDTH = 1200, HEIGHT = 1200;
+const int WIDTH = 600, HEIGHT = 500;
 
 int main ( int arcg, char *argv[])
 {
+    srand(time(NULL));
     // SDL_Init( SDL_INIT_EVERYTHING );
     itemInit();
 
@@ -29,10 +33,24 @@ int main ( int arcg, char *argv[])
     bob.setPos(30, 30);
     bob.setSize(100, 200);
 
+    std::vector <item *> stones;
+    int maxStones = 200;
+    for(int i = 0; i < maxStones; i++)
+    {
+        stones.push_back(new item());
+        stones[i]->setRenderer(screen);
+        stones[i]->loadImage("graphics/stone.png");
+        stones[i]->setPos(rand() % 550, rand() % 450);
+        stones[i]->setSize(50, 50);
+    }
+
+    double angle = 0;
     int speedX = 0;
     int speedY = 0;
 
     bool run = true;
+                        //(renderer, R, G, B, alpha)
+    SDL_SetRenderDrawColor(screen, 30, 180, 20, 255);
     while ( run )
     {
         if ( SDL_PollEvent( &windowEvent ) )
@@ -83,11 +101,19 @@ int main ( int arcg, char *argv[])
         }
         bob.move(speedX, speedY);
 
-        bob.draw();
+        SDL_RenderClear(screen);
+
+        bob.draw(); // draw the player
+        //draw the stones
+        for(int i = 0; i < maxStones; i++)
+        {
+            stones[i]->draw(angle);
+        }
         SDL_RenderPresent(screen);
 
         SDL_Delay(30);
-        SDL_RenderClear(screen);
+        angle++;
+
     }
 
     SDL_DestroyWindow ( window );
